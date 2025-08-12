2efd6064a753353707927f4c1b3a2243d820e443
StephanyMil <stephanymilhomem25@gmail.com>
2025-08-12 07:48:19 -0300

    💡 Remove comments

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',    // O seletor que você usa no HTML (ex: <app-footer></app-footer>)
-  standalone: true,        // Indica que é um componente standalone
-  imports: [],               // Não precisamos importar nada aqui para o footer
-  templateUrl: './footer.html', // Aponta para o arquivo de template HTML
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.html',
 })
 export class FooterComponent {
-  // Propriedade para armazenar o ano atual
   currentYear = new Date().getFullYear();
 }
